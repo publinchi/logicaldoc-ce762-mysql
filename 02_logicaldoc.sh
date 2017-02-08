b94b5b26e0c491991b0fa8320b2e7e4b13b88e03
logicaldoc <info@logicaldoc.com>
2017-02-08 17:36:55 +0100

    Changed xxx file

--- 02_logicaldoc.sh
+++ 02_logicaldoc.sh
@@ -4,7 +4,7 @@ if [ ! -d /opt/logicaldoc/tomcat ]; then
  printf "Installing LogicalDOC\n"
  mysqld_safe & sleep 10s
  /opt/logicaldoc/wait-for-it.sh 127.0.0.1:3306 -t 100
- java -jar /opt/logicaldoc/logicaldoc-installer.jar /opt/logicaldoc/auto-install.xml
+ java -jar /opt/logicaldoc/logicaldoc-installer.jar /opt/logicaldoc/auto-install-762.xml
  killall mysqld_safe & sleep 3s
 else
  printf "Logicaldoc already installed\n"
